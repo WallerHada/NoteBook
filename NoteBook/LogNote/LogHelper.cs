23767838014abb5009f4ed303afd7471f333f201
JG Pi <shenyiwjwze@outlook.com>
2021-08-16 14:11:52 +0800

    新增，日志记录； 新增，http重试；

---
+++ NoteBook/LogNote/LogHelper.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Text;
+
+namespace NoteBook.LogNote;
+public class LogHelper
+{
+    private static readonly string lockKey = "lock";
+    public static void SaveLog(Exception ex, string remark)
+    {
+        StringBuilder sb = new();
+        if (!string.IsNullOrEmpty(remark)) sb.AppendLine(remark);
+        sb.AppendLine(ex.Message);
+        sb.AppendLine(ex.StackTrace);
+        sb.AppendLine(ex.Source);
+        SaveLog("错误日志", sb.ToString());
+    }
+
+    public static void SaveLog(String logName, string msg, params string[] para)
+    {
+        try
+        {
+            if (!Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\logs\\"))
+            {
+                Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\logs\\");
+
+            }
+
+            DateTime now = DateTime.Now;
+            string LogFile = AppDomain.CurrentDomain.BaseDirectory + "\\logs\\" + logName + "_" + now.ToString("yyyy-MM-dd") + ".log";
+            lock (lockKey)
+            {
+                using (FileStream fs = new(LogFile, FileMode.Append, FileAccess.Write))
+                {
+                    byte[] datetimefile = Encoding.Default.GetBytes(logName + "_" + now.ToString("yyyy-MM-dd HH:mm:ss.fff") + ":\r\n");
+                    fs.Write(datetimefile, 0, datetimefile.Length);
+                    if (!string.IsNullOrEmpty(msg))
+                    {
+                        byte[] data = Encoding.Default.GetBytes((para.Length > 0 ? string.Format(msg, para) : msg) + "\r\n==========================================\r\n");
+                        fs.Write(data, 0, data.Length);
+                    }
+                    fs.Flush();
+                }
+            }
+        }
+        catch
+        { }
+    }
+}
---
+++ NoteBook/RetryNote/RetryHelper.cs
@@ -0,0 +1,86 @@
+﻿
+using NoteBook.LogNote;
+
+namespace NoteBook.RetryNote;
+public static class RetryHelper
+{
+    static readonly int sleepMillisecondsTimeout = 1000;
+
+    /// <summary>
+    /// 若發生 Exception (資料庫查詢逾時)，重複執行相同動作
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="handler"></param>
+    /// <param name="retryTimes">預設重試 3次，傳入 0直接 return default(T)</param>
+    /// <returns></returns>
+    public static T Retry<T>(Func<T> handler, int retryTimes = 3)
+    {
+        if (retryTimes <= 0)
+        {
+            return default;
+        }
+
+        try
+        {
+            return handler();
+        }
+        catch (Exception e)
+        {
+            retryTimes--;
+            LogHelper.SaveLog(e, $"剩餘重試次數: {retryTimes}");
+            System.Threading.Thread.Sleep(sleepMillisecondsTimeout);
+            return Retry(handler, retryTimes);
+        }
+    }
+
+    /// <summary>
+    /// 傳入多個動作，遇到 Exception依序執行 (資料庫查詢逾時，改用不同條件查詢)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="handlers"></param>
+    /// <returns></returns>
+    public static T Retry<T>(params Func<T>[] handlers)
+    {
+        for (int i = 0; i < handlers.Length; i++)
+        {
+            var handler = handlers[i];
+            try
+            {
+                return handler();
+            }
+            catch (Exception e)
+            {
+                LogHelper.SaveLog(e, $"第 {i}次執行錯誤(start from 0)");
+                System.Threading.Thread.Sleep(sleepMillisecondsTimeout);
+            }
+        }
+        return default;
+    }
+
+    /// <summary>
+    /// 若發生 Exception (資料庫查詢逾時)，重複執行相同動作
+    /// </summary>
+    /// <param name="handler"></param>
+    /// <param name="retryTimes">預設重試 3次，傳入 0直接 return</param>
+    public static void Retry(Action handler, int retryTimes = 3)
+    {
+        if (retryTimes <= 0)
+        {
+            return;
+        }
+
+        try
+        {
+            handler();
+        }
+        catch (Exception e)
+        {
+            retryTimes--;
+            LogHelper.SaveLog(e, $"剩餘重試次數: {retryTimes}");
+            System.Threading.Thread.Sleep(sleepMillisecondsTimeout);
+            Retry(handler, retryTimes);
+        }
+    }
+
+}
+
